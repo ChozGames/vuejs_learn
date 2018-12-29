7023cb54e1e9961807dcf2269373e58b1e4efd7a
Choz <remy.sainsard@etu.univ-orleans.fr>
2018-12-29 16:02:09 +0100

    Rangement des codes

---
+++ 1_init_vue/index.js
@@ -0,0 +1,9 @@
+var app = new Vue({ 
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+});
+
+// On change le contenu de la variable message
+app.message = "On change le message";
